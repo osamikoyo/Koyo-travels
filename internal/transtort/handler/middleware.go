3931e35bf53b02ed18f9c565d1a87687dae5d185
osamikoyo <v.pylayev@mail.ru>
2025-01-26 12:53:50 +0500

    jwt middleware

--- internal/transtort/handler/middleware.go
+++ internal/transtort/handler/middleware.go
@@ -1,14 +1,30 @@
 package handler
 
 import (
+	"github.com/dgrijalva/jwt-go"
 	"net/http"
+	"os"
 )
 
 type HandlerFunc func (w http.ResponseWriter, r *http.Request) error
 
 func (h Handler) AuthMW(handlerFunc http.HandlerFunc) http.HandlerFunc{
 	return func(w http.ResponseWriter, r *http.Request) {
+		token := r.Header.Get("Authorization")
 
+		if token == "" {
+			http.Error(w, "token not exist", http.StatusUnauthorized)
+		}
+
+		tokens, err := jwt.Parse(token[len("Bearer "):], func(token *jwt.Token) (interface{}, error) {
+            return os.Getenv("JWT_KEY"), nil
+        })
+
+		if err != nil || !tokens.Valid{
+			http.Error(w, "token not valid", http.StatusUnauthorized)
+		}
+
+		handlerFunc.ServeHTTP(w, r)
 	}
 }
 
