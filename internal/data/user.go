ff921050e40bdb8f0a877b40be1b9057895887ab
osamikoyo <v.pylayev@mail.ru>
2025-01-26 11:09:09 +0500

    user register

--- internal/data/user.go
+++ internal/data/user.go
@@ -1,20 +1,37 @@
 package data
 
-import "github.com/osamikoyo/koyo-travels/internal/data/models"
+import (
+	"github.com/osamikoyo/koyo-travels/internal/data/models"
+	"golang.org/x/crypto/bcrypt"
+)
 
 func (s Storage) UserRegister(user models.User) error {
+	password, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil{
+		return err
+	}
+
+	user.Password = string(password)
+
 	return s.AddGorm(user)
 }
 
 func (s Storage) UserLogin(user models.LoginUser) error {
 	var u models.User
 
-	password, err := 
+	password, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil{
+		return err
+	}
 
 	if err := s.Gorm.Where(
 			&models.User{
 				Username: user.Username,
+				Password: string(password),
+			},
+			).Find(&u).Error;err != nil{
+		return err
+	}
 
-			}
-		)
+	return nil
 }
