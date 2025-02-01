b488d11ed97206918c3d356ff50274e6af51ca53
osamikoyo <v.pylayev@mail.ru>
2025-02-01 08:42:36 +0500

    add filters

---
+++ internal/data/models/filter.go
@@ -0,0 +1,17 @@
+package models
+
+type Sort struct {
+	Rait bool
+	Alf  bool
+}
+
+type HotelFilter struct {
+	MaxPrice uint64
+	City     string
+	District string
+}
+
+type Filter struct {
+	Country        string
+	MoreThanItRait float32
+}
